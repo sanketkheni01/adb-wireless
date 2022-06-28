87aea5b4be19a05ed50eb4989ba8e82cbcc828a6
Sanket <sanketkheni.pro@gmail.com>
2022-06-28 16:04:37 +0530

    pair

--- src/index.ts
+++ src/index.ts
@@ -14,4 +14,9 @@ program
   .option('-p, --port <port>', 'The port to connect to', parseInt)
   .action(connect)
 
+// program
+//   .command('pair')
+//   .description('Generate a pairing qr code for your android device')
+//   .action(pair)
+
 program.parse()
--- src/utils/adbConnect.ts
+++ src/utils/adbConnect.ts
@@ -5,7 +5,13 @@ export default function adbConnect(ip: string, port: number) {
   let command = `adb connect ${ip}:${port}`
   exec(command, (err, stdout, stderr) => {
     err && console.log(err)
-    stdout && console.log(stdout)
+    if (stdout) {
+      if (stdout.startsWith('failed to connect')) {
+        console.log(`First pair your device => "adb-wireless pair"`)
+      } else {
+        console.log(stdout)
+      }
+    }
     stderr && console.log(stderr)
   })
 }
